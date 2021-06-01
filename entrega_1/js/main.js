d268374bde1aa5d0e5b87659d1b7e63d0065948b
TomasBasile06 <basile.tomas06@gmail.com>
2021-06-01 09:08:15 -0300

    Agrego while para verificar que la edad es un numero

--- entrega_1/js/main.js
+++ entrega_1/js/main.js
@@ -1,12 +1,17 @@
 let nombre = prompt("Ingrese su nombre: ");
-let edad = Number(prompt("ingrese su edad"));
+let edad = Number(prompt("Ingrese su edad"));
+
+while (isNaN(edad)) {
+    alert("Ingrese un numero como edad.")
+    edad = Number(prompt("Ingrese su edad nuevamente."));
+}
 
 
 if (edad > 18 && nombre.toUpperCase() != "HOMERO") {
-    alert("Bienvenido " + nombre + ", disfrute de la noche!")
+    alert("Bienvenido " + nombre + ", disfrute de la noche!");
 } else if (nombre.toUpperCase() == "HOMERO") {
-    alert("No se admiten Homeros.")
-    alert("Homero Archundia entro porque no se admiten HomeroS.")
+    alert("No se admiten Homeros.");
+    alert("Homero Archundia entro porque no se admiten HomeroS.");
 } else {
-    alert("Vuelva a su casa pequeñe.")
+    alert("Vuelva a su casa pequeñe.");
 }
